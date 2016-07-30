f5305001ebbc3b0521f2fd6eacfebeb747d639b3
枯叶_听海 <5490584810@163.com>
2016-07-30 13:06:34 +0800

    init

---
+++ src/main/java/com/jeremie/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.jeremie;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringKafkaTestApplication.class);
+	}
+
+}
---
+++ src/main/java/com/jeremie/SpringKafkaTestApplication.java
@@ -0,0 +1,12 @@
+package com.jeremie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKafkaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKafkaTestApplication.class, args);
+	}
+}
